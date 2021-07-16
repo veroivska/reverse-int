51e9450c239950eb6245a2cef8dcea0c4bd99c27
Nastia <70321720+veroivska@users.noreply.github.com>
2021-07-16 09:08:25 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 module.exports = function reverse (n) {
-  var a = n;
-var b, sum = 0;
-var z = a;
-while(a > 0)
-{
-  b = a % 10;
-  sum = sum * 10 + b;
-  a = parseInt(a / 10);
-}
-    return sum;
+  const reversed = n.toString().split('').reverse().join(''); 
+  return Math.sign(n) * parseInt(reversed);
 }
